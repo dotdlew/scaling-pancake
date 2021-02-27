d797d6e115f638544a9325c1dddda48886962d4c
Daniel Lewis <daniel.lewis@protonmail.com>
2021-02-27 16:25:44 -0700

    function shift

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,27 +1,15 @@
-// Display today's day and date
-var todayDate = moment().format('dddd, MMM Do YYYY');
-$("#currentDay").html(todayDate);
-
 $(document).ready(function () {
-    // saveBtn click listener 
     $(".saveBtn").on("click", function () {
-        // Get nearby values of the description in JQuery
-        var text = $(this).siblings(".description").val();
         var time = $(this).parent().attr("id");
-
-        // Save text in local storage
+        var text = $(this).siblings(".description").val();
         localStorage.setItem(time, text);
-    })
-   
-    function timeTracker() {
-        //get current number of hours.
-        var timeNow = moment().hour();
+    });
 
-        // loop over time blocks
+    function timeTracker() {
+        var timeNow = moment().hour();
         $(".time-block").each(function () {
             var blockTime = parseInt($(this).attr("id").split("hour")[1]);
 
-            // To check the time and add the classes for background indicators
             if (blockTime < timeNow) {
                 $(this).removeClass("future");
                 $(this).removeClass("present");
@@ -36,12 +24,10 @@ $(document).ready(function () {
                 $(this).removeClass("present");
                 $(this).removeClass("past");
                 $(this).addClass("future");
-
             }
-        })
+        });
     }
 
-    // Get item from local storage if any
     $("#hour8 .description").val(localStorage.getItem("hour8"));
     $("#hour9 .description").val(localStorage.getItem("hour9"));
     $("#hour10 .description").val(localStorage.getItem("hour10"));
@@ -55,3 +41,11 @@ $(document).ready(function () {
 
     timeTracker();
 })
+
+function generateDate() {
+    var timeEl = document.querySelector("#currentDay");
+    var localTime = moment().format('dddd, MMM Do YYYY');
+    timeEl.textContent = localTime;
+}
+
+generateDate();
